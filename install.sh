e34d76acf6186ea6e2398dffe8b3108308b1c4ff
Phil Dufault <phil@dufault.info>
2025-05-08 10:25:12 -0700

    feat: amend capsToggle path, dont install deps

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for file in $(ls -A1 |egrep -v "install.sh|^.git$"); do 
+for file in $(ls -A1 |egrep -v "install.sh|^.git$|deps"); do
   echo -en "symlinking $file...";
   ln -sf $PWD/"$file" ~/"$file"
   echo 'done';
